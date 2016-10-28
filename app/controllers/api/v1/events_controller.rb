6fb414e7c5dac10d04ee07ac292eb4753d61b83e
Owen Horner <owenhorner@live.com.au>
2016-10-29 10:53:34 +1100

    dry up user in events controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,12 +2,11 @@ module Api::V1
   class EventsController < ApplicationController
     def show
       event = Event.find(params[:event][:id])
-      current_user = User.find_by(access_token: params[:user][:access_token])
       render json: {
         event: event,
         group: event.group,
         attendees: event.users,
-        attending: event.participated_by?(current_user)
+        attending: event.participated_by?(user)
       }
     end
 
@@ -35,21 +34,18 @@ module Api::V1
 
     def attending
       event = Event.find(params[:event][:id])
-      current_user = User.find_by(access_token: params[:user][:access_token])
-      event.users << current_user
+      event.users << user
       render json: event.users
     end
 
     def not_attending
       event = Event.find(params[:event][:id])
-      user = User.find_by(access_token: params[:user][:access_token])
       event.users.delete(user)
       render json: event.users
     end
 
     def add_rating
       event = Event.find(params[:event][:id])
-      user = User.find_by(access_token: params[:user][:access_token])
       rating = Rating.find_or_initialize_by(user_id: user.id, event_id: event.id)
       rating.update(rating_score: params[:event][:rating])
       render json: rating
@@ -57,11 +53,19 @@ module Api::V1
 
     def show_rating
       event = Event.find(params[:id])
-      user = User.find(params[:user_id])
-      render json: { rating: event.rating_for(user), average: event.average_rating }
+      user_1 = User.find(params[:user_id])
+      render json: { rating: event.rating_for(user_1), average: event.average_rating }
     end
 
     private
+
+    def user
+      user ||= User.find_by(access_token: user_params.access_token)
+    end
+
+    def user_params
+      params.require(:user).permit(:access_token)
+    end
 
     def create(params)
       event = Event.new
